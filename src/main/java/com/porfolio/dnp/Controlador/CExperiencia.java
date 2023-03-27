e20a626790fe3dc03fd0502335b85e22abd1c247
danieladp88 <73452952+danieladp88@users.noreply.github.com>
2023-03-27 01:01:25 -0300

    cambio en docker

--- src/main/java/com/porfolio/dnp/Controlador/CExperiencia.java
+++ src/main/java/com/porfolio/dnp/Controlador/CExperiencia.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"https://dnpfrontend.web.app","http://localhost:4200"})/*61665*/
+@CrossOrigin(origins = {"https://frontendporfolio-d917f.web.app","http://localhost:4200"})/*61665*/
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
